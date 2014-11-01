3312b62e4b8d095e6a6e6bcdfcccc10fd4693c47
p-kraus <paul.e.kraus@gmail.com>
2014-10-31 17:02:44 -0700

    paperclip and image upload

--- Gemfile
+++ Gemfile
@@ -48,4 +48,5 @@ gem 'spring',        group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+gem 'paperclip', '~> 4.2'
 
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,37 +1,43 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   def index
     @pins = Pin.all
-    respond_with(@pins)
   end
 
   def show
-    respond_with(@pin)
   end
 
   def new
-    @pin = Pin.new
-    respond_with(@pin)
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
-  def create
-    @pin = Pin.new(pin_params)
-    @pin.save
-    respond_with(@pin)
+def create
+    @pin = current_user.pins.build(pin_params)
+
+    respond_to do |format|
+      if @pin.save
+        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
+        format.json { render :show, status: :created, location: @pin }
+      else
+        format.html { render :new }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
     @pin.update(pin_params)
-    respond_with(@pin)
+    redirect_to @pin
   end
 
   def destroy
     @pin.destroy
-    respond_with(@pin)
+    redirect_to_pin_url
   end
 
   private
@@ -39,7 +45,12 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
+  def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
+
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
